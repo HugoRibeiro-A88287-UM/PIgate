23385f9898b476c782959025009cda67a2e5f559
Hugo R <a88287@alunos.uminho.pt>
2021-12-27 16:07:12 +0000

    remove unnecessary packeges in buildroot

--- buildroot-2021.02.5_project/board/raspberrypi/post-build.sh
+++ buildroot-2021.02.5_project/board/raspberrypi/post-build.sh
@@ -9,3 +9,7 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
+
+cp board/raspberrypi4-64/interfaces ${TARGET_DIR}/etc/network/interfaces
+cp board/raspberrypi4-64/inittab ${TARGET_DIR}/etc/inittab
+cp board/raspberrypi4-64/wpa_supplicant.conf ${TARGET_DIR}/etc/wpa_supplicant.conf
